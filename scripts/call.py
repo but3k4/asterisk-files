8596d561729c12bad879c9922cfa3ea43c424c30
but3k4 <cbsfilho@gmail.com>
2013-06-11 20:30:48 -0300

    Fixing call.py

--- scripts/call.py
+++ scripts/call.py
@@ -32,14 +32,14 @@ time.sleep(3)
 
 s.send('Action: originate\r\n')
 s.send('Channel: Sip/%s/%s\r\n' % (TRUNK, TARGET))
-s.send('WaitTime: 10\r\n')
+s.send('WaitTime: 30\r\n')
 s.send('CallerId: unknown\r\n')
 s.send('Application: playback\r\n')
 s.send('Data: monkeys\r\n')
 s.send('Async: true\r\n')
 s.send('Priority: 1\r\n\r\n')
 
-time.sleep(5)
+time.sleep(10)
 
 s.send('Action: Logoff\r\n\r\n')
 s.close()
